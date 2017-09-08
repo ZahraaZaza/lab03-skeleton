b132dc7c27f5639c5e8848bd0927365a709e1077
Zahraa Horeibi <zahraahoreibi@hotmail.com>
2017-09-08 16:18:17 -0400

    wrote the 5 missing lifecycle methods and counters

--- app/src/main/java/course/labs/activitylab/ActivityOne.java
+++ app/src/main/java/course/labs/activitylab/ActivityOne.java
@@ -15,7 +15,16 @@ public class ActivityOne extends Activity {
 
 		// lifecycle counts
 		//TODO: Create 7 counter variables, each corresponding to a different one of the lifecycle callback methods.
-		//TODO:  increment the variables' values when their corresponding lifecycle methods get called.
+		private int createCounter = 0;
+		private int startCounter = 0;
+		private int resumeCounter = 0;
+		private int pauseCounter = 0;
+		private int stopCounter = 0;
+		private int destroyCounter = 0;
+		private int restartCounter = 0;
+
+
+	//TODO:  increment the variables' values when their corresponding lifecycle methods get called.
 		
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +33,11 @@ public class ActivityOne extends Activity {
 			
 			//Log cat print out
 			Log.i(TAG, "onCreate called");
-			
+
 			//TODO: update the appropriate count variable & update the view
+			createCounter++;
+			TextView textView = (TextView) findViewById(R.id.create);
+			textView.setText("onCreate() calls: " + createCounter);
 		}
 
 		@Override
@@ -45,9 +57,72 @@ public class ActivityOne extends Activity {
 			Log.i(TAG, "onStart called");
 			
 			//TODO:  update the appropriate count variable & update the view
+			startCounter++;
+			TextView textView = (TextView) findViewById(R.id.start);
+			textView.setText("onStart() calls: " + startCounter);
 		}
 
 	    // TODO: implement 5 missing lifecycle callback methods
+
+		@Override
+		public void onResume(){
+			super.onResume();
+
+			//Log cat print out
+			Log.i(TAG, "onResume called");
+
+			resumeCounter++;
+			TextView textView = (TextView) findViewById(R.id.resume);
+			textView.setText("onResume() calls: " + resumeCounter);
+		}
+
+		@Override
+		public void onPause(){
+			super.onPause();
+
+			//Log cat print out
+			Log.i(TAG, "onPause called");
+
+			pauseCounter++;
+			TextView textView = (TextView) findViewById(R.id.pause);
+			textView.setText("onPause() calls: " + pauseCounter);
+		}
+
+		@Override
+		public void onStop(){
+			super.onStop();
+
+			//Log cat print out
+			Log.i(TAG, "onStop called");
+
+			stopCounter++;
+			TextView textView = (TextView) findViewById(R.id.stop);
+			textView.setText("onStop() calls: " + stopCounter);
+		}
+
+		@Override
+		public void onDestroy(){
+			super.onDestroy();
+
+			//Log cat print out
+			Log.i(TAG, "onDestroy called");
+
+			destroyCounter++;
+			TextView textView = (TextView) findViewById(R.id.destroy);
+			textView.setText("onDestroy() calls: " + destroyCounter);
+		}
+
+		@Override
+		public void onRestart(){
+			super.onRestart();
+
+			//Log cat print out
+			Log.i(TAG, "onRestart called");
+
+			restartCounter++;
+			TextView textView = (TextView) findViewById(R.id.restart);
+			textView.setText("onRestart() calls: " + restartCounter);
+		}
 
 		@Override
 		public void onSaveInstanceState(Bundle savedInstanceState){
